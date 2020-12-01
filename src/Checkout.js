d04e024344d8c0a7ccc4a792cd15507aff7e38fd
AbdelrahmanNasser1 <61788667+AbdelrahmanNasser1@users.noreply.github.com>
2020-12-02 00:00:05 +0200

    continue

--- src/Checkout.js
+++ src/Checkout.js
@@ -5,7 +5,7 @@ import { useStateValue } from './StateProvider';
 import Subtotal from './Subtotal';
 
 function Checkout() {
-	const [{ basket }] = useStateValue();
+	const [{ basket, user }] = useStateValue();
 	return (
 		<div className="checkout">
 			<div className="checkout__left">
@@ -15,6 +15,7 @@ function Checkout() {
 					alt=""
 				/>
 				<div>
+					<h3>Hello, {user?.email}</h3>
 					<h2 className="checkout__title">your shopping basket</h2>
 					{basket.map((item) => (
 						<CheckoutProduct
